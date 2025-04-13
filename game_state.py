79f9198d9affbff2eb946132f91c75a3ac2b5f40
John Chibuzor Anih <anihcj57@gmail.com>
2025-04-13 17:27:41 +0100

    fix: error in the game_state.py file for game reset

--- game_state.py
+++ game_state.py
@@ -2,6 +2,7 @@ import streamlit as st
 from utils import TicTacToe
 from agents import get_tic_tac_toe_players
 
+
 def initialize_game():
     if "game_started" not in st.session_state:
         st.session_state.game_started = False
@@ -22,4 +23,14 @@ def start_new_game(model_x, model_o):
     st.session_state.game_started = True
     st.session_state.game_over = False
     st.session_state.move_history = []
+    st.rerun()
+    
+def reset_game():
+    keys_to_clear = [
+        "game_started", "game_paused", "move_history",
+        "game_board", "player_x", "player_o",
+        "enter_game", "game_over", "confirm_reset"
+    ]
+    for key in keys_to_clear:
+        st.session_state.pop(key, None)
     st.rerun()
